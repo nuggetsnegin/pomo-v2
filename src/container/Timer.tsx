0f98739b8ff7a375150939bb251de1a97e3c60d5
drop-negin <negin.s@earnwithdrop.com>
2020-10-22 14:22:00 -0400

    formatting timer

--- src/container/Timer.tsx
+++ src/container/Timer.tsx
@@ -7,9 +7,20 @@ export default function Timer() {
   const dispatch = useDispatch();
   const timer = useSelector(getTimer);
 
+  const fancyTimer = (timer: number) => {
+    //http://rocha.la/JavaScript-bitwise-operators-in-practice
+    const minutes: number = ~~((timer % 3600) / 60);
+    const seconds: number = ~~timer % 60;
+    let format: string = "";
+
+    format += "" + minutes + ":" + (seconds < 10 ? "0" : "");
+    format += "" + seconds;
+    return format;
+  };
+
   return (
     <div className="container">
-      <span>timer: {timer}</span>
+      <span>timer: {fancyTimer(timer)}</span>
       <button onClick={() => dispatch(startTimer())}>Start</button>
       <button onClick={() => dispatch(pauseTimer())}>Pause</button>
       <button onClick={() => dispatch(resetTimer())}>Reset</button>
