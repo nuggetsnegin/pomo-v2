8a1bd74e970aca6b24a46866904b301a32e80077
drop-negin <negin.s@earnwithdrop.com>
2020-10-22 13:38:49 -0400

    more goofs

--- src/container/Timer.tsx
+++ src/container/Timer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { startTimer, pauseTimer, resetTimer } from "../actions";
 import { getTimer } from "../selectors";
 
 export default function Timer() {
@@ -9,7 +10,9 @@ export default function Timer() {
   return (
     <div className="container">
       <span>timer: {timer}</span>
-      <button OnClick={()=>dispatch(startTimer)}
+      <button onClick={() => dispatch(startTimer)}>Start</button>
+      <button onClick={() => dispatch(pauseTimer)}>Pause</button>
+      <button onClick={() => dispatch(resetTimer)}>Reset</button>
     </div>
   );
 }
--- src/selectors/index.tsx
+++ src/selectors/index.tsx
@@ -1,5 +1,4 @@
 import { ReducerState } from "../reducers";
-//??
-export const getTimer = (state: ReducerState) => state.elapsedTime;
 
+export const getTimer = (state: ReducerState) => state.elapsedTime;
 export const startTimer = (state: ReducerState) => state.isRunning;
