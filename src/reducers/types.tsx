3fe3177921826080551811c18bb8b1d66169c5a4
drop-negin <negin.s@earnwithdrop.com>
2020-11-16 10:26:15 -0500

    frick

--- src/reducers/types.tsx
+++ src/reducers/types.tsx
@@ -1,7 +1,5 @@
 import { WorkTimerState } from "./work";
-//import { BreakTimerState } from "./break";
 
 export type ReducerState = {
   timer: WorkTimerState;
-  //  breakTimer: BreakTimerState;
 };
